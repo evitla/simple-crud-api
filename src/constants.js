0a2ab41cf856c0806aef54b91a2eee1b168b6785
evitla <s.dinmuhamed@gmail.com>
2021-11-28 16:19:05 +0600

    feat: add constants

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+const HTTP_METHODS = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+};
+
+const HTTP_STATUS_CODE = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER: 500,
+};
+
+module.exports = {
+  HTTP_METHODS,
+  HTTP_STATUS_CODE,
+};
