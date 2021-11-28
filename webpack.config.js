b8dd030399d45aa5ddda97fdf85316702fe1ca69
evitla <s.dinmuhamed@gmail.com>
2021-11-28 11:34:58 +0600

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const { NODE_ENV = 'production' } = process.env;
+
+module.exports = {
+  entry: './src/app.js',
+  mode: NODE_ENV,
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
